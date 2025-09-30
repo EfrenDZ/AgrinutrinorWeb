b8ec9f6d8bbf44630a7d27081f4779ac23a21dc3
EfrenDZ <j.efrendz2004@gmail.com>
2025-09-30 15:09:20 -0700

    todo mas chico

--- Agrinutrinor/front-end/js/carrusel.js
+++ Agrinutrinor/front-end/js/carrusel.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
         type: 'loop',
         drag: 'free',
         focus: 'center',
-        
         // pantallas grandes
         perPage: 4, 
+        perMove:4,
 
         // Configuración para dispositivos móviles
         breakpoints: {
@@ -19,5 +19,27 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         arrows: true, 
         pagination: false,
+    }).mount(window.splide.Extensions);
+
+    new Splide('#mi-carrusel-marcas', {
+        type   : 'loop',    
+        perPage: 4,         
+        perMove: 1,       
+        breakpoints: { 
+            768: {
+                perPage: 3,
+            },
+            480: {
+                perPage: 2,
+            }
+        },
+
+        autoScroll: {
+            speed: 2,
+        },
+        arrows: false, 
+        pagination: false,
+
+        
     }).mount(window.splide.Extensions);
 });
